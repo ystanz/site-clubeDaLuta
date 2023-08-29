57173a98b888709f7f7c7882764f2f3e7fd32b26
Yuri Stanzani <ystanzani@gmail.com>
2023-08-29 19:41:03 -0300

    Initial commit, project setup

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const { write } = require('fs');
+
+function images() {
+    return gulp.src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts'));
+}
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outStyle: 'compressed'}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css'));
+}
+
+exports.default = gulp.parallel(styles, scripts, images);
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', {ignoreInitial: true}, gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', {ignoreInitial: true}, gulp.parallel(scripts));
+    gulp.watch('./src/images/*', {ignoreInitial: true}, gulp.parallel(images));
+}
