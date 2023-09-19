66fe7ff32873125fffa50926638d09258b8b7ca5
Yuri Stanzani <ystanzani@gmail.com>
2023-09-19 13:31:36 -0300

    Project finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,47 +1,55 @@
-// Transição de surgir do hero
 document.addEventListener('DOMContentLoaded', function () {
+    // Animação de surgir o hero
     const transitionClass = 'animation';
     const logo = document.querySelector('[data-logo-transition]');
     logo.classList.add(transitionClass);
     
-    setTimeout(tylerTransition, 1500);
+    setTimeout(tylerTransition, 1000);
     function tylerTransition () {
         const tyler = document.getElementById('tyler-logo');
         const tylerTransition = tyler.classList.add(transitionClass);
     }
-})
-
-// Animação de esconder e mostrar o índice
-const index = document.querySelector('.index');
-document.addEventListener('scroll', function () {
-    let currentPosition = window.scrollY;
     
-    if (currentPosition > 1600) {
-        hideElement();
-    } else {
-        showElement();
+    // Esconder e mostrar o menu
+    const navMenu = document.querySelector('.nav-menu');
+    const navButton = document.querySelector('.nav-button');
+    navButton.addEventListener('click', function () {
+        if (navMenu.style.left === '-100%') {
+            navMenu.style.left = '0px';
+        } else {
+            navMenu.style.left = '-100%';
+        }
+    })
+
+    const heroSection = document.querySelector('.hero')
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const currentPosition = window.scrollY;
+
+        if (currentPosition >= 1121) {
+            navButton.classList.add('nav-button--is-visible')
+        } else {
+            navButton.classList.remove('nav-button--is-visible')
+            navMenu.style.left = '-100%';
+        }
+        // if (currentPosition >= 1121) {
+        //     navButton.style.visibility = 'visible'
+        // } else {
+        //     navButton.style.visibility = 'hidden'
+        // }
+    })
+    
+    // Accordion da sublista do índice
+    const sublistButton = document.querySelectorAll('[data-sublist-button]');
+    
+    for (let i = 0; i < sublistButton.length; i++) {
+        sublistButton[i].addEventListener('click', openSublist);
+    }
+    
+    function openSublist(element) {
+        const openSublistClass = 'nav-menu__sublist--is-open';
+        const elementoPai = element.target.parentNode;
+        elementoPai.classList.toggle(openSublistClass);
     }
 })
-function hideElement() {
-    const index = document.querySelector('.index')
-    index.classList.add('index--is-hiden')
-}
-function showElement() {
-    const index = document.querySelector('.index')
-    index.classList.remove('index--is-hiden')
-}
-
-// Accordion da sublista do índice
-const buttonNav = document.querySelectorAll('[data-drop-button]');
-
-for (let i = 0; i < buttonNav.length; i++) {
-    buttonNav[i].addEventListener('click', openMenu);
-}
-
-function openMenu(element) {
-    const openMenuClass = 'drop-nav--is-open';
-    const elementoPai = element.target.parentNode;
-    elementoPai.classList.toggle(openMenuClass);
-}
-
-
